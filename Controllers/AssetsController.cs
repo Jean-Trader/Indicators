d2092f7b91ccc3764ef90db03ca589f8d65d4af7
Jean-Trader <jeankim1628@gmail.com>
2025-09-16 22:07:18 -0400

    proyecto terminado

--- Controllers/AssetsController.cs
+++ Controllers/AssetsController.cs
@@ -1,11 +1,7 @@
 using Application.DTOs;
-using Application.Interface;
 using Application.Services;
 using Application.ViewModels;
-using Indicators.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using System.Reflection;
 namespace Indicators.Controllers
 {
     public class AssetsController : Controller
@@ -18,16 +14,19 @@ namespace Indicators.Controllers
            
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string? message = null, string? messageType = null)
         {
             ViewBag.AssetNames = _assetsService.GetAllAssets().Select(a => a.Name).ToList();
+            ViewBag.Message = message;
+            ViewBag.MessageType = messageType;
             return View(new List<CalculatedAssetsViewModel>()); 
         }
 
         [HttpPost]        
         public IActionResult Index(string indicatorType)
         {
-           if(!string.IsNullOrWhiteSpace(indicatorType)) 
+          
+            if (!string.IsNullOrWhiteSpace(indicatorType)) 
             {
 
                 List<CalculatedAssetsDto> prediction = IndicatorsPrediccion.Prediccion(indicatorType, _assetsService.GetAllAssets());
@@ -56,21 +55,13 @@ namespace Indicators.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult AddAssets(AddAssetsModelView addmodel)
+        public IActionResult AddAssets(AddAssetsViewModel addmodel)
         {
             try
             {
 
-                Console.WriteLine("POST recibido"); // Debug
-                Console.WriteLine($"ModelState isValid: {ModelState.IsValid}"); // Debug
-                Console.WriteLine($"Nombre recibido: {addmodel?.Name}"); // Debug
-                Console.WriteLine($"Precios recibidos: {addmodel?.PriceHistory?.Count}"); // Debug
-
                 if (ModelState.IsValid)
                 {
-
-
-
                     List<PriceHistoryDto> priceHistory = addmodel.PriceHistory.Select(ph => new PriceHistoryDto
                     {
                         Date = ph.Date,
